672a984c732d11bc11d0f692cde22157a9b68ea4
yorche3 <hyaoki123@gmail.com>
2022-02-07 12:05:46 -0600

    Service Init

---
+++ suggestingfiles/src/main/java/com/suggester/files/suggestingfiles/SuggestingfilesApplication.java
@@ -0,0 +1,13 @@
+package com.suggester.files.suggestingfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggestingfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuggestingfilesApplication.class, args);
+	}
+
+}
---
+++ suggestingfiles/src/test/java/com/suggester/files/suggestingfiles/SuggestingfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suggester.files.suggestingfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuggestingfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
